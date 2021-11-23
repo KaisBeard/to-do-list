cc3fcedd144acb13e192eb01358a7bf4b75c6b48
divya krishna <divya.ravada@gmail.com>
2021-11-23 11:48:35 +0100

    result alignment

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
-import './App.css';
-import Tasklist from "./TaskList"
-import {useState} from "react"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Tab, Alert } from 'react-bootstrap';
-import { ToastBody } from 'react-bootstrap';
-import 'react-tabs/style/react-tabs.css';
+import "./App.css";
+import Tasklist from "./TaskList";
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Tab, Alert } from "react-bootstrap";
+import { ToastBody } from "react-bootstrap";
+import "react-tabs/style/react-tabs.css";
 import { BsCheckSquareFill } from "react-icons/bs";
-import { EmailShareButton,  EmailIcon } from "react-share";
-
+import { EmailShareButton, EmailIcon } from "react-share";
 
 const taskTemplate = {
-  content: '',
-  assignee: '',
-  dueDate: '',
-  priority: '',
-  estTime: ''
-}
+  content: "",
+  assignee: "",
+  dueDate: "",
+  priority: "",
+  estTime: "",
+};
 
 function App() {
-  const [ formData, setFormData ] = useState(taskTemplate);
-  const [ tasks, setTasks ] = useState([]);
-  const [taskEditing, setTaskEditing] = useState(null)
-  const [editingText, setEditingText] = useState("")
+  const [formData, setFormData] = useState(taskTemplate);
+  const [tasks, setTasks] = useState([]);
+  const [taskEditing, setTaskEditing] = useState(null);
+  const [editingText, setEditingText] = useState("");
 
   const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value.trim()
+      [e.target.name]: e.target.value.trim(),
       // content: "make to do list work"
       // assignee: "fey kai"
       // Trimming any whitespace [e.target.name]: e.target.value.trim()
@@ -34,12 +33,11 @@ function App() {
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    setTasks([...tasks, formData])
-    
+    e.preventDefault();
+    setTasks([...tasks, formData]);
   };
 
-  // BEGIN of task priority colors: 
+  // BEGIN of task priority colors:
 
   // const taskPriority = [
   //   {name: 'Very High'},
@@ -58,62 +56,105 @@ function App() {
 
   // END od task priority colors - the functionality is not working
 
-  
-
-  console.log(formData)
+  console.log(formData);
   return (
-    
     <div>
       <div className="container">
         <h1>WBS Group Project - Assignments:</h1>
         <div className="content-box">
           <section>
-                <label>Task<input type="text" name="content" onChange={handleChange}/></label>
-                <label> Assignee
-                  <select name="assignee" onChange={handleChange}>
-                    <option value=""></option>
-                    <option value="Kai">Kai</option>
-                    <option value="Fran">Fran</option>
-                    <option value="Divya">Divya</option>
-                </select>
-                </label>
+            <label>
+              Task
+              <input type="text" name="content" onChange={handleChange} />
+            </label>
+            <label>
+              {" "}
+              Assignee
+              <select name="assignee" onChange={handleChange}>
+                <option value=""></option>
+                <option value="Kai">Kai</option>
+                <option value="Fran">Fran</option>
+                <option value="Divya">Divya</option>
+              </select>
+            </label>
 
-                <label>Due<input type="date" name="dueDate" onChange={handleChange} /></label>
-                <label> Priority
-                  <select name="priority" onChange={handleChange}>
-                    <option value=""></option>
-                    <option value="Very High" style={{ color: 'red' }}>Very High</option>
-                    <option value="High" style={{ color: 'orange' }}>High</option>
-                    <option value="Medium" style={{ color: 'yellow' }}>Medium</option>
-                    <option value="Low" style={{ color: 'green' }}>Low</option>
-                </select>
-                </label>
-                <label>Estimated time (h)<input type="number" min="0" max="12" step="0.25" name="dueDate" onChange={handleChange} /></label>
-                <label>Comment<input type="text" name="comment" placeholder="This task is about?" onChange={handleChange} /></label>
+            <label>
+              Due
+              <input type="date" name="dueDate" onChange={handleChange} />
+            </label>
+            <label>
+              {" "}
+              Priority
+              <select name="priority" onChange={handleChange}>
+                <option value=""></option>
+                <option value="Very High" style={{ color: "red" }}>
+                  Very High
+                </option>
+                <option value="High" style={{ color: "orange" }}>
+                  High
+                </option>
+                <option value="Medium" style={{ color: "yellow" }}>
+                  Medium
+                </option>
+                <option value="Low" style={{ color: "green" }}>
+                  Low
+                </option>
+              </select>
+            </label>
+            <label>
+              Estimated time (h)
+              <input
+                type="number"
+                min="0"
+                max="12"
+                step="0.25"
+                name="estTime"
+                onChange={handleChange}
+              />
+            </label>
+            <label>
+              Comment
+              <input
+                type="text"
+                name="comment"
+                placeholder="This task is about?"
+                onChange={handleChange}
+              />
+            </label>
 
-                <Button onClick={handleSubmit} variant="success" size="sm">Add new Task</Button>   
+            <Button onClick={handleSubmit} variant="success" size="sm">
+              Add new Task
+            </Button>
 
-                {/* edit button - still need to put functionality */}
+            {/* edit button - still need to put functionality */}
 
-                <div className="edit-task">
-                  <input type="text" onChange={(a) => setEditingText(a.target.value)}
-                    value={editingText} />
-                  <Button onClick={() => setTaskEditing()} variant="warning" size="sm">Edit task</Button>
-                </div> 
-                
-              <div>
-                
-                <Tasklist tasks={tasks} />
-              </div>
-            </section>
-          </div>
+            <div className="edit-task">
+              <input
+                type="text"
+                onChange={(a) => setEditingText(a.target.value)}
+                value={editingText}
+              />
+              <Button
+                onClick={() => setTaskEditing()}
+                variant="warning"
+                size="sm"
+              >
+                Edit task
+              </Button>
+            </div>
+
+            <div>
+              <Tasklist tasks={tasks} />
+            </div>
+          </section>
         </div>
-        </div>
-        );   
-      }
-      /*
+      </div>
+    </div>
+  );
+}
+/*
       <CreateEntry randomTask="do something"
             Task={sendForm} />
       */
 
-    export default App;
+export default App;
--- src/TaskList/index.js
+++ src/TaskList/index.js
@@ -1,27 +1,32 @@
 import "./styles.css";
-import {useState} from "react";
-import {React} from "react";
+import { useState } from "react";
+import { React } from "react";
 import "bootstrap-icons/font/bootstrap-icons.css";
 //import {createElement} from "react";
 //import {ReactDOM} from 'react-dom';
 
 //const [taskList, setTasklist] = useState(null)
 
-
-
-function Tasklist ({ tasks }) {
-    
-    return (
-      <div className="Tasklist">
-        <h1>Todo List</h1>
-        <div>{tasks.map((a) => <div>Task: {a.content} -- Assignee: {a.assignee} -- Priority: {a.priority} -- Due: {a.dueDate} -- Estimated time: {a.estTime} -- Comment: {a.comment} <hr></hr> </div>)}</div>
-            
+function Tasklist({ tasks }) {
+  return (
+    <div className="Tasklist">
+      <h1>Todo List</h1>
+      <div>
+        {tasks.map((a) => (
+          <div className="resultAlign btn btn-outline-dark m-2 p-2">
+            <div>Task: {a.content}</div>
+            <div>Assignee: {a.assignee}</div>
+            <div>Priority: {a.priority}</div>
+            <div>Due: {a.dueDate}</div>
+            <div>Estimated time: {a.estTime}</div>
+            <div>Comment: {a.comment}</div>
+          </div>
+        ))}
       </div>
-    );
-  }
-  //<div>Your task:{props.Task}</div>
+    </div>
+  );
+}
+//<div>Your task:{props.Task}</div>
 //<div>{tasks.map(a => <div>{a}</div>)}</div>
-
-
 
 export default Tasklist;
